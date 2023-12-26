1f10ab0de9279087af6989ef6769a52ee3686dc3
Abhikkumar619 <abisheky194@gmail.com>
2023-12-26 07:57:19 +0545

    model trainer completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from wafer_project import logger
 from wafer_project.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from wafer_project.pipeline.stage_02_data_transformation import DataTransformationPipeline
+from wafer_project.pipeline.stage_03_model_trainer import ModelTrainerPipeline
 
 
 """
@@ -25,3 +26,15 @@ try:
     logger.info(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>stage {stage_name} Ended >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 except Exception as e: 
     raise e
+
+stage_name= "Model_Trainer"
+
+if __name__ == '__main__': 
+    logger.info(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> stage {stage_name} Started >>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    try: 
+        obj=ModelTrainerPipeline()
+        obj.main()
+    except Exception as e: 
+        raise e
+    logger.info(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> stage {stage_name} Ended >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    
--- src/wafer_project/pipeline/stage_03_model_trainer.py
+++ src/wafer_project/pipeline/stage_03_model_trainer.py
@@ -1,5 +1,10 @@
+from wafer_project.config.configuration import ConfigurationManager
+from wafer_project.components.Model_Trainer import ModelTrainer
+from wafer_project import logger
 
 
+
+stage_name= "Model_Trainer"
 
 class ModelTrainerPipeline: 
     def __init__(self): 
@@ -10,10 +15,17 @@ class ModelTrainerPipeline:
             config_model_config=configmanager.get_model_trainer_config()
             model_trainer=ModelTrainer(config_model_config)
             model_trainer.initate_model_trainer()
-        except Excption as e: 
+        except Exception as e: 
             raise e
 
-
-if __main__ == '__name__': 
+if __name__ == '__main__': 
+    logger.info(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> stage {stage_name} Started >>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    try: 
+        obj=ModelTrainerPipeline()
+        obj.main()
+    except Exception as e: 
+        raise e
+    logger.info(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> stage {stage_name} Ended >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    
     
     
